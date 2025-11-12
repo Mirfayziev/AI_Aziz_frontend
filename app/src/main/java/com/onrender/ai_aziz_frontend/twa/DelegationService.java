fe3967c218526b889695017c71cfd5c6587927d9
Aziz_Fayziev <fayziev978@mail.ru>
2025-11-12 11:34:56 -0800

    1

---
+++ app/src/main/java/com/onrender/ai_aziz_frontend/twa/DelegationService.java
@@ -0,0 +1,22 @@
+package com.onrender.ai_aziz_frontend.twa;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+import com.google.androidbrowserhelper.playbilling.digitalgoods.DigitalGoodsRequestHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+            registerExtraCommandHandler(new DigitalGoodsRequestHandler(getApplicationContext()));
+        
+    }
+}
+
