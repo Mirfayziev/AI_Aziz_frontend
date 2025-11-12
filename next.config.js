2ac37c89528893881d0d5475f881464eaabcfe89
Aziz_Fayziev <fayziev978@mail.ru>
2025-11-12 12:28:42 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,17 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_BACKEND_URL: "https://ai-aziz-backend.onrender.com"
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: "/manifest.json",
+        headers: [
+          { key: "Content-Type", value: "application/manifest+json" }
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
