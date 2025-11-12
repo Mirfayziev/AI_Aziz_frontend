dc0842eae5cfa9648e5185908a77a19816dae1b4
Aziz_Fayziev <fayziev978@mail.ru>
2025-11-12 10:48:34 -0800

    1

---
+++ manifest.josn.js
@@ -0,0 +1,24 @@
+{
+  "name": "AI Aziz",
+  "short_name": "AI Aziz",
+  "description": "AI Aziz — sizning shaxsiy sun’iy intellekt yordamchingiz.",
+  "start_url": "https://ai-aziz-frontend.onrender.com/index.html",
+  "scope": "https://ai-aziz-frontend.onrender.com/",
+  "id": "https://ai-aziz-frontend.onrender.com/",
+  "display": "standalone",
+  "background_color": "#000000",
+  "theme_color": "#000000",
+  "orientation": "portrait-primary",
+  "icons": [
+    {
+      "src": "https://ai-aziz-frontend.onrender.com/logo192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "https://ai-aziz-frontend.onrender.com/logo512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
