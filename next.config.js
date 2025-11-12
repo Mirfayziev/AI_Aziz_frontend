d064c2b4163f85e78599a084c0a3bf8d08524762
Aziz_Fayziev <fayziev978@mail.ru>
2025-11-12 12:11:06 -0800

    1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  async headers() {
+    return [
+      {
+        source: '/manifest.json',
+        headers: [
+          { key: 'Content-Type', value: 'application/manifest+json' },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
